8b283428d3f572c3a186a5cd6cbdaeb9bfbc44cb
twqabc <twqabc@gmail.com>
2021-06-20 09:48:55 -0400

    v0.5

--- src/store/index.js
+++ src/store/index.js
@@ -12,14 +12,36 @@ export default createStore({
         about: `
  # 联系方式
 
-- 手机：18061887416
-- Email：twqabssc@gmail.com
-- QQ/微信号：xltccipa
+- 手机：18061887413
+- Email：twqabc@gmail.com
+- QQ/微信号：xltipa
 
 # 个人信息
 
-- 田伟奇/女/2001
-- 您共事。
+- 田伟奇/男/2001
+- 大专/南京财经高等职业技术学校计算机网络专业
+- Github：<a> http://github.com/twqabc </a>
+- Segmentfault: <a> https://segmentfault.com/u/twqabc </a>
+- 期望职位：软件测试实习生
+- 期望薪资：税前月薪2k~，特别喜欢的公司可例外
+- 期望城市：南京
+
+
+# 开源项目
+- [STU](https://github.com/twqabc/vueBlog)：个人博客网站
+- [WXYZ](https://github.com/twqabc/gp-qxs)：栖霞山网站
+# 技能清单
+        以下均为我熟练使用的技能
+- Web开发：Node
+- Web框架：Django
+- 前端框架：Bootstrap/Vue JS
+- 前端工具：Sass
+- 数据库相关：Mongodb/SQLite
+- 版本管理、文档和自动化部署工具：Git
+- 单元测试：SimpleTest
+- 云和开放平台：微信应用开发 
+# 致谢
+        感谢您花时间阅读我的简历，期待能有机会和您共事。
         `  
     },
     mutations: {
